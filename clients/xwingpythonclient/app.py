f0b61e84f59755227add3fa0c5306d5d12720609
Ewan Slater <ewan.slater@gmail.com>
2017-07-14 21:23:02 +0100

    Raw SQL example working

--- clients/xwingpythonclient/app.py
+++ clients/xwingpythonclient/app.py
@@ -1,4 +1,6 @@
 import requests
+import MySQLdb
+import os
 from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
@@ -21,7 +23,26 @@ def read_db_SQLAlchemy_example():
 
 @app.route('/read_db_raw_SQL_example')
 def read_db_raw_SQL_example():
-    return "I want my DB!"
+    cursor = get_db().cursor()
+    sql = "SELECT * FROM SampleTable"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    rows = ""
+    for row in results:
+        rows = rows + ','.join(row) + "<br/>"
+    return rows
+
+def get_db():
+    my_user = os.environ["MYSQLCS_USER_NAME"]
+    my_passwd = os.environ["MYSQLCS_USER_PASSWORD"]
+    my_host, port_db = os.environ["MYSQLCS_CONNECT_STRING"].split(":")
+    my_port_str, my_db = port_db.split("/")
+    my_port = int(my_port_str)
+    return MySQLdb.connect(host=my_host,
+        user=my_user,
+        passwd=my_passwd,
+        port=my_port,
+        db=my_db)
 
 if __name__ == '__main__':
-    app.run(debug=True);
+    app.run(debug=True,host='0.0.0.0', port=int(os.environ["PORT"]));
