34ab946a759a3564c4810822e43ffabe4fb0ef29
Ewan Slater <ewan.slater@gmail.com>
2017-08-24 22:26:49 +0100

    added default config values

--- clients/xwingpythonclient/config.py
+++ clients/xwingpythonclient/config.py
@@ -2,10 +2,11 @@ import os
 
 class Configuration(object):
     DEBUG = True
-    PORT = int(os.environ['PORT'])
+    PORT = int(os.getenv('PORT','22222'))
     HOST = '0.0.0.0'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql://' \
-            + os.environ['MYSQLCS_USER_NAME'] + ':' \
-            + os.environ['MYSQLCS_USER_PASSWORD'] + '@' \
-            + os.environ['MYSQLCS_CONNECT_STRING']
+            + os.getenv('MYSQLCS_USER_NAME','root') + ':' \
+            + os.getenv('MYSQLCS_USER_PASSWORD','Welcome_1') + '@' \
+            + os.getenv('MYSQLCS_CONNECT_STRING', \
+            '127.0.0.1:3306/sample_db')
